d1d669fc52c58934c0d3bd261ef311843956e5d0
Alison Hight <afhight@gmail.com>
2016-02-09 20:25:04 -0500

    second update

--- app/controllers/rock_controller.rb
+++ app/controllers/rock_controller.rb
@@ -1,4 +1,9 @@
 class RockController < ApplicationController
   def roll
+  	@songs = Song.order(rating: :desc).limit(5)
+  end
+
+  def rollartists
+  	@artists = Artist.order(rating: :desc).limit(5)
   end
 end
--- app/models/artist.rb
+++ app/models/artist.rb
@@ -4,4 +4,5 @@ class Artist < ActiveRecord::Base
 	has_many :songs
 
 	validates :name, presence: true, uniqueness: true
+	validates :rating, presence: true
 end
